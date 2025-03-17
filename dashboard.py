9144c35d9dccfc1a077b9271025c038d7fb86d5b
AdamJavier28 <adamjavier26@student.ub.ac.id>
2025-03-17 15:18:28 +0800

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import matplotlib as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+sns.set(style='dark')
+
+def create_hourly_rentals_df(df):
+    hourly_rentals_df = hour_df.groupby("hr").agg({
+        "cnt": "sum"
+    }).reset_index()
+    hourly_rentals_df.rename(columns={
+        "hr": "hour",
+        "cnt": "total_rentals"
+    }, inplace=True)
+
+    return hourly_rentals_df
+
+def create_monthly_trend_df(df):
+    monthly_trend_df = day_df.resample(rule='M', on='dteday').agg({
+        "cnt": "sum"
+    })
+    monthly_trend_df.index = monthly_trend_df.index.strftime('%Y-%m')
+    monthly_trend_df = monthly_trend_df.reset_index()
+    monthly_trend_df.rename(columns={
+        "dteday": "year_month",
+        "cnt": "total_rentals"
+    }, inplace=True)
+    return monthly_trend_df
+
+day_df = pd.read_csv("day_df.csv")
+hour_df = pd.read_csv("hour_df.csv")
+
+
+monthly_trend_df = create_monthly_trend_df()
+hourly_trend_df = create_hourly_rentals_df()
+
+st.subheader('Tren Penyewaan Sepeda per Bulan')
+
+max_month = monthly_trend_df.loc[monthly_trend_df["total_rentals"].idxmax()]
+st.metric("Tren teritinggi pada Bulan", value=max_month)
+
+fig, ax = plt.subplots(figsize=(10, 5))
+ax.plot(monthly_trend_df["year_month"], monthly_trend_df["total_rentals"], marker="o", linestyle="-", color="#72BCD4")
+ax.set_xlabel("Bulan", fontsize=12)
+ax.set_ylabel("Jumlah Penyewaan Sepeda", fontsize=12)
+ax.tick_params(axis='x', rotation=45) 
+st.pyplot(fig)
+
+st.subheader("Tren Penyewaan Sepeda Berdasarkan Jam")
+max_hour = hourly_trend_df.loc[hourly_trend_df["total_rentals"].idxmax()]
+st.metric("Waktu dengan Penyewaan Tertinggi", value=max_hour)
+
+fig, ax = plt.subplots(figsize=(10, 5))
+ax.plot(hourly_trend_df["hour"], hourly_trend_df["total_rentals"], 
+        marker="o", linestyle="-", color="#72BCD4")
+ax.set_xticks(range(0, 24)) 
+
+st.pyplot(fig)
