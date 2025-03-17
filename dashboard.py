581172907a29e1a94d5c005812ff22da13406208
AdamJavier28 <adamjavier26@student.ub.ac.id>
2025-03-17 23:19:17 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -77,8 +77,8 @@ if selected_option == "Tren Penyewaan Jam":
 if selected_option == "Distribusi Penyewaan Sepeda Berdasarkan Jumlah Per Hari":
     rental_counts = create_bins_df(hour_df)
     st.subheader("Distribusi Penyewaan Sepeda Berdasarkan Kuantitas")
-    plt.figure(figsize=(8, 8))
-    plt.pie(rental_counts, labels=rental_counts.index, autopct="%1.1f%%", colors=["#ff9999", "#66b3ff", "#99ff99", "#ffcc99", "#c2c2f0"])
-    plt.title("Distribusi Penyewaan Sepeda Berdasarkan Kuantitas")
-    plt.show()
+    fig, ax = plt.subplots(figsize=(8, 8))
+    ax.pie(rental_counts, labels=rental_counts.index, autopct="%1.1f%%", 
+       colors=["#ff9999", "#66b3ff", "#99ff99"], startangle=140)
+    st.pyplot(fig)
 
